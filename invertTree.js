a6363a8646334b7ae10d143d0f211ff1b453f003
seanmull <kimlan065@gmail.com>
2022-06-20 21:26:46 +1000

    added invertTree

--- invertTree.js
+++ invertTree.js
@@ -6,14 +6,19 @@ class TreeNode {
     }
 }
 
-first = new TreeNode(4);
-second = new TreeNode(2);
-third = new TreeNode(1);
-fourth = new TreeNode(3);
-fifth = new TreeNode(7);
-sixth = new TreeNode(6);
-seventh = new TreeNode(9);
+let first = new TreeNode(4);
+let second = new TreeNode(2);
+let third = new TreeNode(1);
+let fourth = new TreeNode(3);
+let fifth = new TreeNode(7);
+let sixth = new TreeNode(6);
+let seventh = new TreeNode(9);
 
+//       4
+//   2      7 
+// 1   3  6   9
+
+//inorder 1 2 3 4 6 7 9
 first.left = second;
 first.right = fifth;
 second.left = third;
@@ -21,16 +26,9 @@ second.right = fourth;
 fifth.left = sixth;
 fifth.right = seventh
 
-postOrderTraversal(first);
-
-invertTree(first);
-inOrderTraversal(first);
-
-function invertTree(root) {
+let invertTree = (root) => {
     if (root !== null) {
-        tmp = root.left;
-        root.left = root.right;
-        root.right = tmp;
+        [root.left, root.right] = [root.right, root.left]
         invertTree(root.left);
         invertTree(root.right);
     }
@@ -44,9 +42,31 @@ function inOrderTraversal(node) {
     inOrderTraversal(node.right);
 }
 
+//       4
+//   2      7 
+// 1   3  6   9
+
+//      4
+//  7       2
+// 9  6   3   1
+
+// call stack
+//   
+//  null 
+// 
+//  
+
 function postOrderTraversal(node) {
     if (node === null) return;
     postOrderTraversal(node.right);
     console.log(node.val);
     postOrderTraversal(node.left);
 }
+
+console.log("In order traversal before inversion")
+inOrderTraversal(first);
+invertTree(first);
+console.log("In order traversal after inversion")
+inOrderTraversal(first);
+
+
