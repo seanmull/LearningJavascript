c46ed18d29d2a81aaa440b7affb1540dba913730
sean mull <kimlan065@gmail.com>
2022-06-20 16:52:38 +1000

    added tree

---
+++ tree.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+class Tree {
+  constructor(value) {
+    this.root = new Node(value)
+  }
+
+  insert(value) {
+    let addedNode = new Node(value)
+    if (!this.root) {
+      this.root = addedNode
+      return this
+    }
+    let currentNode = this.root
+    while (true) {
+      if (currentNode.value > value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left
+        } else {
+          currentNode.left = addedNode
+          return this
+        }
+      } else if (currentNode.value < value) {
+        if (currentNode.right) {
+          currentNode = currentNode.right
+        } else {
+          currentNode.right = addedNode
+          return this
+        }
+      } else {
+        return undefined
+      }
+    }
+  }
+  find(value) {
+    let current = this.root
+    while (current !== null) {
+      if (current.value === value) return true
+      else if (current.value > value) current = current.left
+      else if (current.value < value) current = current.right
+    }
+    return false
+  }
+}
+
+let tree = new Tree(2)
+tree.insert(3)
+tree.insert(1)
+tree.insert(4)
+tree.root.right.right.value
+console.log(tree)
+tree.find(3)
+tree.find(2)
+tree.find(1)
+tree.find(9)
