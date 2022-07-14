a0c09db66a7d10e5c68faabca664c0bf66f60a5a
sean mull <kimlan065@gmail.com>
2022-07-15 08:50:29 +1000

    fixed 3 sum

--- 3sum.js
+++ 3sum.js
@@ -17,25 +17,33 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-   let results = []
-   nums.sort() 
-   for(let i = 0; i < nums.length; i++){
-    if(i != 0 && nums[i] === nums[i - 1]) continue
-    let j = i + 1
-    let k = nums.length - 1
-    while(j < k){
-      if(nums[i] + nums[j] + nums[k] === 0){
-        results.push([nums[i], nums[j], nums[k]])
-        j++
-        while (j < k && nums[j] == nums[j - 1]) j++
-      }else if(nums[i] + nums[j] + nums[k] < 0){
-        j++
-      }else{
-        k--
+   let result = []
+   nums.sort()
+   // stop 2 places from the end that is where lo and hi are
+   for(let i = 0; i < nums.length - 2; i++){
+    // if its less then 0 theres no way it can sum to zero since its the earlier element
+    // in a sorted array.  Slight optimization
+    if(nums[i] > 0) break
+    // skip repeats after the first index, this ensures we don't get out of bounds error
+    if(i > 0 && nums[i] === nums[i - 1]) continue
+    let lo = i + 1, hi = nums.length - 1
+    while(lo < hi){
+      let sum = nums[i] + nums[lo] + nums[hi]
+      if(sum < 0) lo++
+      else if(sum > 0) hi--
+      else{
+        result.push([nums[i], nums[lo], nums[hi]])
+        // ensure we don't get repeats after we add the first element
+        // check both lo and hi for repeats
+        while(lo < hi && nums[lo] === nums[lo + 1]) lo++
+        while(lo < hi && nums[hi] === nums[hi - 1]) hi--
+        // we still need to check other elements within this lo - hi interval
+        lo++
+        hi--
       }
     }
    }
-   return results
+   return result
 };
 
 
