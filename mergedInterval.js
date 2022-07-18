5253550d351173bb47d09650a2bf249a4fde284f
seanmull <kimlan065@gmail.com>
2022-07-18 14:22:26 +1000

    Update mergedInterval.js

--- mergedInterval.js
+++ mergedInterval.js
@@ -3,18 +3,23 @@
 // interavals = [[1,4],[4,5]]
 let interavals = [[1,4], [2,3]]
 
-interavals.sort((a,b) => a[0] - b[0])
-interavals
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+   intervals.sort((a,b) => a[0] - b[0])
 
-let mergedIntervals = [interavals[0]];
-mergedIntervals
-let j = 0;
-for(let i = 1; i < interavals.length; i++){
-  if(interavals[i][0] <= mergedIntervals[j][1]){
-    mergedIntervals[j][1] = Math.max(interavals[i][1], mergedIntervals[j][1]);
-  }else{
-    mergedIntervals.push(interavals[i]);
-    j++;
-  }
-}
-mergedIntervals
+    let mergedIntervals = [intervals[0]];
+
+    let j = 0;
+    for(let i = 1; i < intervals.length; i++){
+        if(intervals[i][0] <= mergedIntervals[j][1]){
+            mergedIntervals[j][1] = Math.max(intervals[i][1], mergedIntervals[j][1]);
+        }else{
+            mergedIntervals.push(intervals[i]);
+            j++;
+        }
+    }
+    return mergedIntervals 
+};
