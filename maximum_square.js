188cd847c8251ba1f0f9ee99490c8b90ebda5fff
seanmull <kimlan065@gmail.com>
2022-07-18 13:23:02 +1000

    Update maximum_square.js

--- maximum_square.js
+++ maximum_square.js
@@ -3,21 +3,22 @@
  * @return {number}
  */
 var maximalSquare = function(matrix) {
-    let r = matrix.length + 1
-    let c = matrix[0].length + 1
-    let dp = Array(r).fill(Array(c).fill(0))
-    let ml = 0
-    for(let i = 1; i < r - 1; i++){
-        for(let j = 1; j < c -1; j++){
-            if(matrix[i - 1][j - 1] === "1"){
-                dp[i][j] = Math.min(
-                           dp[i - 1][j],
-                           dp[i - 1][j - 1],
-                           dp[i][j - 1]) + 1
-            }
-            ml = Math.max(ml, dp[i][j])
-            
-        }
-    }
-    return ml * ml
+    let rows = matrix.length;
+    let cols = rows ? matrix[0].length : 0;
+    let max = 0;
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            let left = col === 0 ? 0 : matrix[row][col - 1],
+                up = row === 0 ? 0 : matrix[row - 1][col], 
+                back = row && col ? matrix[row - 1][col - 1] : 0;
+            console.log(left + up + back)
+            if (matrix[row][col] == 1) {
+                matrix[row][col] = Math.min(left, up, back) + 1;
+            } else {
+                matrix[row][col] = 0;
+            };
+            max = max > matrix[row][col] ? max : matrix[row][col];
+        };
+    };
+    return max * max;
 };
