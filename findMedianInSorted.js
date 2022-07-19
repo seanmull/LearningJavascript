4e922fc6c0d8be31870b9f9a79cdac4d94428f30
seanmull <kimlan065@gmail.com>
2022-07-19 16:06:54 +1000

    Update findMedianInSorted.js

--- findMedianInSorted.js
+++ findMedianInSorted.js
@@ -3,8 +3,31 @@
  * @param {number[]} nums2
  * @return {number}
  */
+let merge = (arr1, arr2) => {
+    let results = []
+    let p1 = 0, p2 = 0
+    while(p1 < arr1.length || p2 < arr2.length){
+        if(p1 >= arr1.length){
+            results.push(arr2[p2])
+            p2++
+        }else if(p2 >= arr2.length){
+            results.push(arr1[p1])
+            p1++
+        }else if(arr1[p1] >= arr2[p2]){
+            results.push(arr2[p2])
+            p2++
+        }else{
+            results.push(arr1[p1])
+            p1++
+        }
+    }
+    return results
+}
+
+
+
 var findMedianSortedArrays = function(nums1, nums2) {
-    let sortedArray = [...nums1, ...nums2].sort((a, b) => a - b)
+    let sortedArray = merge(nums1, nums2)
     let n = Math.floor(sortedArray.length/2)
     if(sortedArray.length % 2 === 1){
         return sortedArray[n]
@@ -13,3 +36,4 @@ var findMedianSortedArrays = function(nums1, nums2) {
                sortedArray[n]) / 2
     }
 };
+
