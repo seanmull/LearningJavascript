527484720718714de141a85134e22cfbd5de0b97
sean mull <kimlan065@gmail.com>
2022-07-22 10:39:03 +1000

    update zigzaglevel

--- binaryZigZag.js
+++ binaryZigZag.js
@@ -17,24 +17,26 @@ function TreeNode(val, left, right) {
      this.right = (right===undefined ? null : right)
 }
 
-let one = new TreeNode(3)
-let two = new TreeNode(9)
-let three = new TreeNode(20)
-let four = new TreeNode(15)
-let five = new TreeNode(7)
-let six = new TreeNode(8)
-let seven = new TreeNode(19)
-let nine = new TreeNode(90)
-one.left = two
-one.right = three
-three.left = four
-three.right = five
-four.left = six
-four.right = seven
-five.left = nine
+let one = null
+// let one = new TreeNode()
+// let two = new TreeNode(9)
+// let three = new TreeNode(20)
+// let four = new TreeNode(15)
+// let five = new TreeNode(7)
+// let six = new TreeNode(8)
+// let seven = new TreeNode(19)
+// let nine = new TreeNode(90)
+// one.left = two
+// one.right = three
+// three.left = four
+// three.right = five
+// four.left = six
+// four.right = seven
+// five.left = nine
 
 
 var zigzagLevelOrder = function(root) {
+  if(root === null) return []
   let c = root, q = [c], r = [], l, f, len, flag = true
   while(q.length !== 0){
     len = q.length
