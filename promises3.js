7f0c71e21acccdea9d4db9d50490ea7fbbdc9a63
seanmull <kimlan065@gmail.com>
2022-08-27 22:59:11 +1000

    added promises

---
+++ promises3.js
@@ -0,0 +1,26 @@
+// using a resolved promise, the 'then' block will be triggered instantly,
+// but its handlers will be triggered asynchronously as demonstrated by the console.logs
+
+const resolvedProm = Promise.resolve(33);
+
+const thenProm = resolvedProm.then((value) => {
+    console.log(
+        "this gets called after the end of the main stack. the value received and returned is: ",
+        value,
+      );
+    return value;
+});
+
+// instantly logging the value of thenProm
+console.log(thenProm);
+// using setTimeout we can postpone the execution of a function to the moment the stack is empty
+
+setTimeout(() => {
+    console.log(thenProm);
+});
+
+// logs, in order:
+// Promise {[[PromiseStatus]]: "pending", [[PromiseResult]]: undefined}
+// "this gets called after the end of the main stack. the value received and returned is: 33"
+// Promise {[[PromiseStatus]]: "resolved", [[PromiseResult]]: 33}
+
